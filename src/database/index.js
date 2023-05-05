c246d836d68a741eb1b35705494cfcbdb21e2869
heribeiro <helena.correa84@gmail.com>
2023-05-05 18:32:33 -0300

    CREATE DATABASE

---
+++ src/database/index.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { Client } = require('pg');
+
+const client = new Client({
+  host: 'localhost',
+  port: 5432,
+  user: 'root',
+  password: 'root',
+  database: 'mycontacts',
+
+});
+
+client.connect();
+
+exports.query = async (query) => {
+  const { rows } = await client.query(query);
+  return rows;
+};
